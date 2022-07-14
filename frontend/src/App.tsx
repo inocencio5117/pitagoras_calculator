b24fc93d7f30f0088f73d0683d2e96ced9efa603
Vinicius Inocencio <vicnicius2009@gmail.com>
2022-07-14 13:02:32 -0300

    Feat: adding inital layout html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,27 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Calculadora do Teorema de Pitágoras</h1>
+      <div>
+        <h2>Texto sobre o teorema de pitágoras</h2>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est voluptas
+          quos amet minima corrupti neque saepe distinctio porro expedita rem
+          esse quo deserunt, non blanditiis. Voluptates unde optio beatae cum?
+        </p>
+      </div>
+      <div>
+        {/* <img src="" alt="" /> */}
+        <input type="checkbox" name="area-toggle" id="area-toggle" />
+        <div>
+          <span>hip</span>
+          <span>ladoA</span>
+          <span>ladoB</span>
+        </div>
+        <button>calcular</button>
+        <button>Limpar campos</button>
+        <div>resolução do problema</div>
+      </div>
     </div>
   );
 }
