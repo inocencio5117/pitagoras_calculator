2af94ab580a8a705b01332c39ad02f51ffa45e6c
Vinicius Inocencio <vicnicius2009@gmail.com>
2022-07-15 11:19:46 -0300

    Feat: creating request logic

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,38 @@
-import { useState } from "react";
+import React, { useState } from "react";
+
+import axios from "axios";
+
 import pythagorasImg from "./assets/pythagorasTheorem.png";
 import "./App.css";
 
 function App() {
+  const [hip, setHip] = useState("");
+  const [sideA, setSideA] = useState("");
+  const [sideB, setSideB] = useState("");
+  const [result, setResult] = useState("");
+
+  function handleInput(
+    element: React.ChangeEvent<HTMLInputElement>,
+    stateFunction: React.Dispatch<React.SetStateAction<string>>
+  ) {
+    const value = element.target.value;
+    stateFunction(value);
+  }
+
+  function handleClearButton() {
+    setHip("");
+    setSideA("");
+    setSideB("");
+  }
+
+  async function handleCalculate() {
+    const request = await axios(
+      `http://127.0.0.1:5000/calculate?side=sideB&hip=5&sideA=3`
+    );
+
+    console.log(request.data);
+  }
+
   return (
     <div className="App">
       <h1 className="App-title">Calculadora do Teorema de Pitágoras</h1>
@@ -30,29 +60,47 @@ function App() {
           <div className="form-wrapper">
             <div className="form-child">
               <label htmlFor="hip">Valor da Hipotenusa:</label>
-              <input type="text" name="hip" placeholder="?" />
+              <input
+                type="number"
+                name="hip"
+                placeholder="?"
+                value={hip}
+                onChange={(el) => handleInput(el, setHip)}
+              />
             </div>
 
             <hr />
 
             <div className="form-child">
               <label htmlFor="sideA">Valor do Cateto a:</label>
-              <input type="text" name="sideA" placeholder="3" />
+              <input
+                type="number"
+                name="sideA"
+                placeholder="3"
+                value={sideA}
+                onChange={(el) => handleInput(el, setSideA)}
+              />
             </div>
 
             <hr />
 
             <div className="form-child">
               <label htmlFor="sideB">Valor do Cateto b:</label>
-              <input type="text" name="sideB" placeholder="4" />
+              <input
+                type="number"
+                name="sideB"
+                placeholder="4"
+                value={sideB}
+                onChange={(el) => handleInput(el, setSideB)}
+              />
             </div>
           </div>
 
           <hr />
 
           <div className="buttons-container">
-            <button>Calcular</button>
-            <button>Limpar campos</button>
+            <button onClick={handleCalculate}>Calcular</button>
+            <button onClick={handleClearButton}>Limpar campos</button>
           </div>
 
           {/* <div>resolução do problema</div> */}
